84086d8ece67e932e6852fd156724f8e5d8ebe95
HabibJR <smhabib.abir2030@gmail.com>
2022-10-10 17:00:41 +0200

    add sortable dragable and dropable

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
   console.log("hi there");
 
-  $("#sortable").sortable();
-  
+  /* $(".cube").draggable({
+    containment: "parent",
+    cursor:"grabbing",
+    opacity: 0.5,
+    //snap:true,
+    //snapTolerance:100
+  });*/
+
+  //$(".droppable").droppable();
+
+  /*$("#task-todo-list").sortable({
+    connectWith: "#task-in-progress",
+  });
+
+  $("#task-in-progress").sortable({
+    connectWith: "#task-todo-list",
+  });*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $(".sortable").sortable({
+    connectWith: ".sortable",
+  });
 
 });
 // open a modal box function
